cde7fe27c130e283a5a2f1729bf3fb2c63cba63d
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-11-25 15:26:52 +0300

    bug fix 2

---
+++ SemTask003/Program.cs
@@ -0,0 +1,40 @@
+﻿// Написать пррограмму, которая на вход принимает номер дня недели и выдет его название.
+
+Console.WriteLine("Enter the number of the day of the week:");
+
+int number = int.Parse(Console.ReadLine());
+
+if(number == 1)
+{
+    Console.WriteLine("Monday");
+}
+
+if(number == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+
+if(number == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+
+if(number == 4)
+{
+    Console.WriteLine("Thursday");
+}
+
+if(number == 5)
+{
+    Console.WriteLine("Friday");
+}
+
+if(number == 6)
+{
+    Console.WriteLine("Saturday");
+}
+
+if(number == 7)
+{
+    Console.WriteLine("Sunday");
+}
