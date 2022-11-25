f10744e7161f2af331e9d233b307b26567192a51
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-11-25 10:48:51 +0300

    Bug fix

---
+++ SemTask002/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать пррограмму, которая на вход принимает два числа и проверяет является ли первое число квадратом второго.
+
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+
+if(number1 == number2 * number2)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
