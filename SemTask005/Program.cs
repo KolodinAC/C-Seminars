7716259864113901c5678aafe043002ea5f11225
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-11-27 17:08:30 +0300

    SemTask005 added

---
+++ SemTask005/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу, которая выводит случайное число из отрезка [10, 99] и 
+// показывает первую цифру числа(надо разделить на 10 чтоб округлилось), 
+//последнюю цифру(надо посмотреть остаток от деления на 10)и наибольшую цифру числа.
+
+Random random = new Random();
+
+int number = random.Next(10, 100);
+
+Console.WriteLine(number);
+
+int firstNumber = number / 10;
+int lastNumber = number % 10;
+
+if(firstNumber > lastNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    Console.WriteLine(lastNumber);
+}
