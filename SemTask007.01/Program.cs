8f2020786fabedaa0e4e8b6f00acfbce19d71c17
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-11-28 11:15:49 +0300

    SemTask007.01 added TryParse check

--- SemTask007.01/Program.cs
+++ SemTask007.01/Program.cs
@@ -3,17 +3,26 @@
 
 Console.WriteLine("Введите желаемое число отличное от нуля:");
 
-int number = int.Parse(Console.ReadLine());
-int a = 7;
-int b = 23;
-int rest = (number % a) + (number % b);
+bool isParsed = int.TryParse(Console.ReadLine(), out int number); // проверка на то ввели ли пользователь цифры или же по ошибке буквы. TryParse проверяет возможность считать цифры. bool задает переменную isParsed которая принимает значения True если получилось считать цифры или False если пользователь ввел вместо цифр буквы и система не смогла их считать.
+//int number = int.Parse(Console.ReadLine());
 
-if(rest == 0)
+if(!isParsed)
 {
-    Console.WriteLine("Ваше число кратно 7 и 23.");
+    Console.WriteLine("Введено неправильное значение, вводите числа!");
 }
 else
 {
-    Console.WriteLine("Ваше число не кратно 7 и 23, остаток ");
-    Console.WriteLine(rest);
+    int a = 7;
+    int b = 23;
+    int rest = (number % a) + (number % b);
+
+    if(rest == 0)
+    {
+        Console.WriteLine("Ваше число кратно 7 и 23.");
+    }
+    else
+    {
+        Console.WriteLine("Ваше число не кратно 7 и 23, остаток ");
+        Console.WriteLine(rest);
+    }
 }
