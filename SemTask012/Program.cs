391acd2f04288442744be6af82133525f2bf2db6
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-01 08:30:26 +0300

    SemTask012 fixed

--- SemTask012/Program.cs
+++ SemTask012/Program.cs
@@ -1,13 +1,31 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
 // int[] array =
 
-Console.WriteLine("Введите желаемое число");
 
-int N = int.Parse(Console.ReadLine());
+int N = InputInt();
 
 int[] squaresTable = GetSquaresTab(N);
 
 PrintArray(squaresTable);
+
+
+
+
+
+int InputInt()
+{
+    System.Console.WriteLine("Введите желаемое целое число:");
+    bool isNum = int.TryParse(Console.ReadLine(), out int N);
+    if(isNum)
+    {
+        return N;
+    }
+    else
+    {
+        System.Console.WriteLine("Вы ввели некорректное значение");
+        return -1;
+    }
+}
 
 int[] GetSquaresTab(int N)
 {
@@ -19,13 +37,14 @@ int[] GetSquaresTab(int N)
     return arr;
 }
 
-void PrintArray (int[] arr)
+void PrintArray(int[] arr)
 {
     int l = arr.Length;
     int i = 0;
     while (i < l)
     {
-        Console.Write($" {arr[i]}");
+        Console.Write($"{arr[i]} ");
         i++;
     }
+    Console.WriteLine();
 }
