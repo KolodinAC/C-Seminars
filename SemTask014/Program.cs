31628a375adf5298a80f38584f4c178d8367e1de
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-05 14:41:59 +0300

    task 14 done

---
+++ SemTask014/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+
+
+int number = UserUnput();
+
+int product = GetProdOfNumbersInInterval(number);
+
+Console.WriteLine(product);
+
+
+int UserUnput()
+{
+    System.Console.WriteLine("Введите число:");
+    bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+    if (!isParsed || number < 1)
+    {
+        Console.WriteLine("Incorrect number");
+        
+    }
+    return number;
+}
+
+int GetProdOfNumbersInInterval(int number)
+{
+    int prod = 1;
+    for (var i = 1; i <= number; i++)
+    {
+        prod = prod * i;
+    }
+    return prod;
+}
