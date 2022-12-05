ce96967f4909e92f8d0a12c746f5c91c6ff27970
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-05 14:10:34 +0300

    SemTAsk013 done

--- SemTask013_testing_code/Program.cs
+++ SemTask013_testing_code/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 24: Напишите программу, которая принимает на вход число
+// (А) и выдаёт сумму чисел от 1 до А.
+//7 -> 28 4 -> 10 8 -> 36
+/*bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+
+if (!isParsed || number < 1)
+{
+    Console.WriteLine("Incorrect number");
+    return;
+}
+
+int sum = GetSumOfNumbersInInterval(number);
+Console.WriteLine(sum);*/
+
+void TestPositiveGetSumOfNumbersInInterval()
+{
+    int[] numbers = new int[] { 1, 2, 3, 4 };
+
+    int[] expected = new int[] { 1, 3, 6, 10 };
+
+    for (var i = 0; i < numbers.Length; i++)
+    {
+        int actual = GetSumOfNumbersInInterval(numbers[i]);
+
+        if (actual != expected[i])
+        {
+            Console.WriteLine($"Ожидали получить {expected[i]} а получили {actual}");
+        }
+        else
+        {
+            Console.WriteLine("okay");
+        }
+    }
+}
+
+// void Test()
+// {
+//     int[] lengths = new int[] { 1, 2, 3, 4 };
+
+//     int[] expected = new int[] { 1, 2, 3, 4 };
+
+//     for (var i = 0; i < lengths.Length; i++)
+//     {
+//         int[] actual = Fill(lengths[i]);
+//         if (expected[i] == actual.Length)
+//         {
+
+//         }
+//     }
+// }
+
+int result = GetSumOfNumbersInInterval(number);
+
+int GetSumOfNumbersInInterval(int number)
+{
+    int sum = 0;
+    for (var i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
