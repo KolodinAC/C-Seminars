5b2ca7a19ea7c17a99207a7c055d20894a004061
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-12 19:19:50 +0300

    semtask 017 added

---
+++ SemTask018/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие 
+// отрицательные, и наоборот.
+
+int userLength = UserInput();
+
+float[] userArray = GenerateArray(userLength);
+
+Console.Write("Ваш массив:  ");
+
+PrintArray(userArray);
+
+userArray = ChangeNegativeToPositive(userArray);
+
+Console.Write("Ваш массив после замены отрицательных чисел на положительные:  ");
+
+PrintArray(userArray);
+
+
+
+
+
+
+
+int UserInput()
+{
+    Console.Write("Введите желаемый размер массива:\t");
+    bool isNum = int.TryParse(Console.ReadLine(), out int num);
+    if (isNum && num > 0)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("Вы ввели некорректное значение");
+        return -1;
+    }
+}
+
+float[] GenerateArray(int userLength)
+{
+    Random random = new Random();
+    float[] array = new float[userLength];
+
+    for (var i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-100, 101);
+    }
+    return array;
+}
+
+void PrintArray(float[] collection)
+{
+    Console.WriteLine("[{0}]", string.Join(", ", collection));
+}
+
+float[] ChangeNegativeToPositive(float[] collection)
+{
+    float temp = 0;
+    for (int index = 0; index < collection.Length; index++)
+    {
+        if (collection[index] < 0)
+        {
+            collection[index] = collection[index] * -1;
+        }
+    }
+    return collection;
+}
