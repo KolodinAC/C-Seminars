71826e682f10ac8e8a3b561578f7451c460463ed
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-12 20:08:28 +0300

    sem task 20 done

---
+++ SemTask020/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+float[] userArray = GenerateArray(123);
+
+PrintArray(userArray);
+
+int countOfNum = GetCountOfNumbFromSegment(userArray);
+
+Console.WriteLine(countOfNum);
+
+
+
+float[] GenerateArray(int userLength)
+{
+    Random random = new Random();
+    float[] array = new float[userLength];
+
+    for (var i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, userLength);
+    }
+    return array;
+}
+
+void PrintArray(float[] collection)
+{
+    Console.WriteLine("[{0}]", string.Join(", ", collection));
+}
+
+int GetCountOfNumbFromSegment(float[] collection)
+{
+    int count = 0;
+    for (int index = 0; index < collection.Length; index++)
+    {
+        if(collection[index] >= 10 & collection[index] <= 99)
+        {
+            count++;
+        }
+    }
+    return count;
+}
