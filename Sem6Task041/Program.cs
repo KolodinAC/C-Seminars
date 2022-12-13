fd5827880e93744f36fb31ed6fb230473565c980
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-13 18:26:34 +0300

    fix

---
+++ Sem6Task041/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное. 45 -> 101101
+
+
+using static Common.Helper;
+
+int numDec = UserInput("Введите десятичное число для преобразования в двоичное: ");
+int numBin = GetBinaryFromDecimal(numDec);
+Console.WriteLine(numBin);
+
+
+int GetBinaryFromDecimal(int num)
+{
+    int newNum = 0;
+    while (num > 0)
+    {
+        int rest = num % 2;
+        newNum = newNum * 10 + rest;
+        num = num / 10;  
+    }
+    return newNum;
+}
