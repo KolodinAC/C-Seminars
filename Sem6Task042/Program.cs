984196761553e9a44f7a70d10e2575420d532ffe
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-15 15:58:02 +0300

    fix

---
+++ Sem6Task042/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1
+
+
+
+using static Common.Helper;
+
+int userNumber = UserInput("Введите желаемое количество чисел Фибоначчи: ");
+
+int[] Fibonacci (int userNum)
+{
+    int[] arr = new int[userNum];
+    arr[0] = 0;
+    if()
+}
