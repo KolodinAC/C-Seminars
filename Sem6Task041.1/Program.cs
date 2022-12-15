05f088b9985acf763860c58183ed13081cd40825
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-15 10:06:15 +0300

    sem task 41.1 added

---
+++ Sem6Task041.1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное. 45 -> 101101
+
+// Решение через строку - десятичное число переводится в двоичное в виде строки:
+
+
+using static Common.Helper;
+
+int numDec = UserInput("Введите десятичное число для преобразования в двоичное: ");
+string res = DecToBin(numDec);
+System.Console.WriteLine("Ваше число в двоичной системе: " + res);
+
+string DecToBin(int num) // метод записывает результат в виде строки
+{
+    string binNum = string.Empty;
+    
+    while(num != 0)
+    {
+       binNum = num % 2 + binNum;
+       num = num / 2; 
+    }
+    return binNum;
+}
