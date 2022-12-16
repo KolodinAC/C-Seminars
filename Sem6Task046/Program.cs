9a794564391d0df5503d93f4b5ae5cae6d0809ad
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-16 20:23:52 +0300

    TASK 46 fix

--- Sem6Task046/Program.cs
+++ Sem6Task046/Program.cs
@@ -12,17 +12,17 @@ Print2DArray(array);
 
 
 
-int[,] CreateRandom2DArray(int countOfRows, int countOfColumns)
-{
-    Random random = new Random();
-    int[,] array = new int[countOfRows, countOfColumns];
+// int[,] CreateRandom2DArray(int countOfRows, int countOfColumns)
+// {
+//     Random random = new Random();
+//     int[,] array = new int[countOfRows, countOfColumns];
 
-    for (var i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = random.Next(-10, 11);
-        }
-    }
-    return array;
-}
+//     for (var i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = random.Next(-10, 11);
+//         }
+//     }
+//     return array;
+// }
