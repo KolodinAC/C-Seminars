06e6c3589af98e024da96100f3b54374ccb7f647
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-16 12:34:34 +0300

    semtask48 done

---
+++ Sem6Task048/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₙₙ = m+n. 
+// Выведите полученный массив на экран. m = 3, n = 4.
+
+
+using static Common.Helper;
+
+int[,] matrix = new int[3, 4];
+FillTwoDimArray(matrix);
+PrintTwoDimArray(matrix);
+
+
+
+void FillTwoDimArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintTwoDimArray(int[,] matr)           
+{
+    for (int i = 0; i < matr.GetLength(0); i++)           
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)      
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
