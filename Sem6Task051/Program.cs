f4ef49a133f9ca9d3c5db46d0c07eaeca42f1ecd
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-17 16:20:25 +0300

    fix

--- Sem6Task051/Program.cs
+++ Sem6Task051/Program.cs
@@ -8,24 +8,3 @@ int n = UserInput("Введите желаемое количество стол
 
 int[,] newArray = CreateRandom2DArray(m, n);
 Print2DArray(newArray);
-int[,] arr = GetSquareForUneven(newArray);
-System.Console.WriteLine();
-Print2DArray(arr);
-
-int[,] GetSquareForUneven(int[,] matr)
-{
-    for (int i = 0; i < matr.GetLength(0); i++)
-    {
-        if (i % 2 != 0)
-        {
-            for (int j = 0; j < matr.GetLength(1); j++)
-            {
-                if (j % 2 != 0)
-                {
-                    matr[i, j] = (int)(Math.Pow(matr[i, j], 2));
-                }
-            }
-        }
-    }
-    return matr;
-}
