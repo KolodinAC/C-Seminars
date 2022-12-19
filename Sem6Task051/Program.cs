1028fd0957055735bc2ead58d6dd4bf590aa27ea
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-19 20:26:45 +0300

    task 51 done

--- Sem6Task051/Program.cs
+++ Sem6Task051/Program.cs
@@ -8,3 +8,27 @@ int n = UserInput("Введите желаемое количество стол
 
 int[,] newArray = CreateRandom2DArray(m, n);
 Print2DArray(newArray);
+System.Console.WriteLine();
+int sumOfElem = GetSumOfMainDiag(newArray);
+System.Console.Write("Сумма элементов по главной диагонали составляет: ");
+System.Console.WriteLine(sumOfElem);
+
+
+
+
+
+int GetSumOfMainDiag(int[,] matr)
+{
+    int sum = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum += matr[i, j];
+            }
+        }
+    }
+    return sum;
+}
